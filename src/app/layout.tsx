a03e9387d05e25caef46d0e7a8747a1b780bf475
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-10-25 18:51:43 +0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,7 @@ import { Suspense } from "react";
 import "../styles/globals.css";
 
 export const metadata: Metadata = {
-  title:
-    "Профессиональная плазменная резка металла в Екатеринбурге - Ликос",
+  title: "Профессиональная плазменная резка металла в Екатеринбурге - Ликос",
   description:
     "Высокоточная плазменная резка металла любой сложности. Быстро, качественно, по доступным ценам. Калькулятор стоимости онлайн.",
   generator: "v0.app",
@@ -23,6 +22,24 @@ export default function RootLayout({
       <body
         className={`font-sans ${GeistSans.variable} ${GeistMono.variable} antialiased`}
       >
+        <div
+          dangerouslySetInnerHTML={{
+            __html: `<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104847440', 'ym');
+
+    ym(104847440, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/104847440" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+`,
+          }}
+        />
         <Suspense fallback={null}>{children}</Suspense>
       </body>
     </html>
